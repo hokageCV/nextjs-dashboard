347ef68793476cfcf7fd77bc5b4aee7ea24e6ff5
hokageCV <chaitanyavaru@gmail.com>
2023-07-11 21:07:29 +0530

    initial commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,28 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
-  ],
-  theme: {
-    extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
+    content: [
+        "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+        "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+        "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    ],
+    theme: {
+        extend: {
+            colors: {
+                black: "#000000",
+                white: {
+                    1: "#ffffff",
+                    2: "#f5f5f5",
+                    3: "#eaeaea",
+                },
+                gray: {
+                    1: "#666666",
+                    2: "#858585",
+                    3: "#999999",
+                    4: "#b0b0b0",
+                },
+                link: "#346bd4",
+            },
+        },
     },
-  },
-  plugins: [],
-}
+    plugins: [],
+};
