1b0ea20000e36b74a4441fe93893bc1701889bda
hokageCV <chaitanyavaru@gmail.com>
2023-07-12 19:39:27 +0530

    add schedule box

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -85,9 +85,45 @@ export default function Dashboard() {
                 </div>
 
                 {/* pie chart and schedule */}
-                <div>
-                    <div>pie chart</div>
-                    <div>schedule</div>
+                <div className="flex flex-row gap-10">
+                    {/* pie chart */}
+                    <div className="flex-1 max-w-[480px] bg-white-1 px-10 py-7.5 rounded-xl">
+                        <div className="flex flex-row items-center justify-center">
+                            <p className="mr-auto text-lg font-bold">Top products</p>
+                            <p className="text-xs text-gray-2">May-June 2021</p>
+                            <img src="/arrow_down.svg" alt="down arrow" className="ml-1" />
+                        </div>
+                    </div>
+
+                    {/* schedule  */}
+                    <div className="flex-1 max-w-[480px] bg-white-1 px-10 py-7.5 rounded-xl">
+                        <div className="flex flex-row items-center justify-center mb-6.4">
+                            <p className="mr-auto text-lg font-bold">Today's schedule</p>
+                            <p className="text-xs text-gray-2">See All</p>
+                            <img src="/arrow_right.svg" alt="down arrow" className="ml-1" />
+                        </div>
+
+                        <div className="flex flex-row mb-3 font-sans">
+                            <div className="h-[66px] w-[5px] bg-green-300 mr-3.5"></div>
+                            <div>
+                                <p className="font-bold text-sm text-gray-1">
+                                    Meeting with suppliers from Kuta Bali
+                                </p>
+                                <p className="font-xs text-gray-3">14:00 - 15:00</p>
+                                <p className="font-xs text-gray-3">at Sunset Road, Kuta, Bali</p>
+                            </div>
+                        </div>
+                        <div className="flex flex-row font-sans">
+                            <div className="h-[66px] w-[5px] bg-green-300 mr-3.5"></div>
+                            <div>
+                                <p className="font-bold text-sm text-gray-1">
+                                    Check operation at Giga Factory 1
+                                </p>
+                                <p className="font-xs text-gray-3">18:00 - 20:00</p>
+                                <p className="font-xs text-gray-3">at Central Jakarta</p>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
